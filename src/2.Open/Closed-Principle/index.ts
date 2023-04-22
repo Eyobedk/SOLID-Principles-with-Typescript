4888ef0e2f84174c96fd99135b3580774e1574d7
Eyobedk <Eyobedkebede10@gmail.com>
2023-04-22 07:07:21 +0000

    open/closed principle

---
+++ src/2.Open/Closed-Principle/index.ts
@@ -0,0 +1,43 @@
+interface Database {
+    getUserData(userId: string): UserData;
+}
+
+interface UserData {
+    name: string;
+    email: string;
+    phone: string;
+    // ... other user datas
+}
+
+class User {
+    constructor(private db: Database) {}
+  
+    public getUser(userId: string): UserData {
+      return this.db.getUserData(userId);
+    }
+}
+  
+
+
+  
+class MongoDatabase implements Database {
+    public getUserData(userId: string): UserData {
+        // ... logic to retrieve user data from our MongoDB database
+        return {name: "name_from_mongo", email: "email", phone: "phone"}
+    }
+}
+  
+class PostgresDatabase implements Database {
+    public getUserData(userId: string): UserData {
+        // ... logic to retrieve user data from our PostgreSQL database
+        return {name: "name_from_postgresql", email: "email", phone: "phone"}
+    }
+} 
+
+// const user1 = new User(new MongoDatabase());
+// console.log(user1.getUser("101A"));
+// // { name: 'name_from_mongo', email: 'email', phone: 'phone' }
+
+// const user2 = new User(new PostgresDatabase());
+// console.log(user2.getUser("101B"));
+// // { name: 'name_from_postgresql', email: 'email', phone: 'phone' }
---
+++ src/index.ts
@@ -0,0 +1,106 @@
+// class User {
+//     constructor(private name: string) {}
+  
+//     getName() {
+//       return this.name;
+//     }
+// }
+  
+// class PremiumUser extends User {
+//     private rewardPoints: number;
+  
+//     constructor(name: string, rewardPoints: number) {
+//       super(name);
+//       this.rewardPoints = rewardPoints;
+//     }
+  
+//     getRewardPoints() {
+//       return this.rewardPoints;
+//     }
+// }  
+
+// const premiumUser = new PremiumUser("eyobed", 112);
+
+// console.log("reward:", premiumUser.getRewardPoints())
+// console.log("name:", premiumUser.getName());
+
+// import User from "./entity/user";
+
+// interface UserRepository {
+//     saveUser(user: User): Promise<void>;
+//     getUserById(userId: string): Promise<User | undefined>;
+//     updateUser(user: User): Promise<void>;
+//     deleteUser(userId: string): Promise<void>;
+//   }
+  
+// class InMemoryUserRepository implements UserRepository {
+//     private users: User[];
+  
+//     constructor() {
+//       this.users = [];
+//     }
+  
+//     async saveUser(user: User): Promise<void> {
+//       this.users.push(user);
+//     }
+  
+//     async getUserById(userId: string): Promise<User | undefined> {
+//       return this.users.find(user => user.id === userId);
+//     }
+  
+//     async updateUser(user: User): Promise<void> {
+//       const existingUser = await this.getUserById(user.id);
+//       if (existingUser) {
+//         Object.assign(existingUser, user);
+//       }
+//     }
+  
+//     async deleteUser(userId: string): Promise<void> {
+//       this.users = this.users.filter(user => user.id !== userId);
+//     }
+// }
+  
+// class UserManagementService {
+//     private readonly userRepository: UserRepository;
+
+//     constructor(userRepository: UserRepository) {
+//         this.userRepository = userRepository;
+//     }
+
+//     async createUser(user: User): Promise<void> {
+//         // Do some validation or other business logic
+//         await this.userRepository.saveUser(user);
+//     }
+
+//     async getUserById(userId: string): Promise<User | undefined> {
+//         // Do some validation or other business logic
+//         return await this.userRepository.getUserById(userId);
+//     }
+
+//     async updateUser(user: User): Promise<void> {
+//         // Do some validation or other business logic
+//         await this.userRepository.updateUser(user);
+//     }
+
+//     async deleteUser(userId: string): Promise<void> {
+//         // Do some validation or other business logic
+//         await this.userRepository.deleteUser(userId);
+//     }
+// }
+  
+
+// async function run(){
+//     const userService = new UserManagementService(new InMemoryUserRepository());
+
+//     await userService.createUser(new User("101A", "eyobed", "eyobedkebded@gmail.com","09XXXXX"));
+   
+//     console.log( await userService.getUserById("101A"))
+//     // User {
+//     //      id: '101A',
+//     //      name: 'eyobed',
+//     //      email: 'eyobedkebded@gmail.com',
+//     //      phone: '09XXXXX'
+//     //    }
+// }
+
+// run();
